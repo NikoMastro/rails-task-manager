97f2a7e57bb8774aa0258cbf2796dd7e6b5bde46
pierre nikolas contessotto <pierrenikolas@gmail.com>
2024-08-06 14:13:09 +0900

    half

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,46 @@
 class TasksController < ApplicationController
-  def tasks
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+  @tasks = Task.find(params[:title])
+  end
+
+  def new
+  @task = Task.new
+  end
+
+  def create
+    @task = task.new(task_params)
+    if @task.save
+      redirect_to task_path(@task)
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @task= task.find(params[:id])
+  end
+
+  def update
+    @task= task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to task_path(@task)
+    else
+      render 'edit', status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @task= task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
+  end
+  private
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
